12469882944e98813ce8e813ae9aa5552156a1c7
davidxphp <davidxphp@gmail.com>
2021-01-24 21:28:13 -0300

    frontend

---
+++ src/components/template/Footer.js
@@ -0,0 +1,10 @@
+import './Footer.css'
+import React from 'react'
+
+export default props =>
+    <footer className="footer">
+        <span>
+            Desenvolvido com <i className="fa fa-heart text-danger"></i> por
+            <strong className="text-danger"> Central System<span className="text-primary"> - Teste </span> - React</strong>
+        </span>
+    </footer>
